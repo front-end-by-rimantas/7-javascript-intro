7eff11c3d73bbaae2fd78e3ceb027fc2d6f0f09f
Rimantas <belauzas@yahoo.com>
2019-04-01 10:35:04 +0300

    Console.log and basic logic

---
+++ js/main.js
@@ -0,0 +1,37 @@
+console.log( 3.1415 );
+console.log( '3.1415' );
+console.log( true );
+console.log( false );
+
+console.log( 3.1415 + 3.1415 );
+console.log( '3.1415' + '3.1415' );
+console.log( true + true );
+console.log( false + false );
+
+console.log( 3.1415 + '3.1415' );
+console.log( true + '3.1415' );
+console.log( false + '3.1415' );
+
+console.log( '3.1415' + 3.1415 );
+console.log( '3.1415' + true );
+console.log( '3.1415' + false );
+
+console.log( 1 + false );
+console.log( false + 1 );
+console.log( 1 + true );
+console.log( true + 1 );
+
+
+console.log( 2 + 2 + '2' + 2 + 2 );
+//2+2+'2'+2+2
+//4+'2'+2+2
+//'42'+2+2
+//'422'+2
+//'4222'
+
+console.log( 2 + 2 + '2' + 2 * 2 );
+//2+2+'2'+2*2
+//2+2+'2'+4
+//4+'2'+4
+//'42'+4
+//'424'
